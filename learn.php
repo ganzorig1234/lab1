d5affaa0f6d7cc5ba4132f019133bc51d5d6c379
ganzo1234 <38126567+ganzo1234@users.noreply.github.com>
2018-09-18 16:30:37 +0800

    second commit

---
+++ learn.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>learn</title>
+</head>
+<body>
+	<input type="button" value="click me!" id = "button"/>
+	<div id = "button_feedback"></div>
+	<script type="text/javascript" src = "js/jquery.js"></script>
+	<script type="text/javascript" src = "js/jfunc.js"></script>
+</body>
+</html>
